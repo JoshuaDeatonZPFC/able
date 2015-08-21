8c9697bcc5116d6397a3321154aa23a6a3d07a7a
JoshuaDeaton <jdeaton422@tech-rift.com>
2015-08-20 22:01:45 -0500

    Define Control Parameters

--- able.js
+++ able.js
@@ -1,8 +1,14 @@
 var noble = require('noble');
 var serial = require('serialport').SerialPort;
-//var serialport = new serial("/dev/ttyS2", {
-//    baudrate: 9600
-//});
+var serialport = new serial("/dev/ttyS2", {
+    baudrate: 9600
+});
+
+var AIL = "s0",
+    ELE = "s1",
+    THR = "s2",
+    RUD = "s3",
+    AUX = "s4";
 
 console.log("App Start");
 
@@ -13,7 +19,44 @@ noble.on('stateChange', function (state) {
 console.log("Scan");
 noble.startScanning();
 noble.on('discover', function (peripheral) {
-    if (peripheral.advertisement.localName === "BKON") {
-        console.log('Device Found');
+    if (peripheral.advertisement.localName === "ABLE") {
+
+        serialport.write(AIL + " 1000", function (err, results) {
+            if (err) {
+                console.log(err);
+            } else if (results) {
+                console.log(results);
+            }
+        });
+        serialport.write(ELE + " 1000", function (err, results) {
+            if (err) {
+                console.log(err);
+            } else if (results) {
+                console.log(results);
+            }
+        });
+        serialport.write(THR + " 1000", function (err, results) {
+            if (err) {
+                console.log(err);
+            } else if (results) {
+                console.log(results);
+            }
+        });
+        serialport.write(RUD + " 1000", function (err, results) {
+            if (err) {
+                console.log(err);
+            } else if (results) {
+                console.log(results);
+            }
+        });
+        serialport.write(AUX + " 1000", function (err, results) {
+            if (err) {
+                console.log(err);
+            } else if (results) {
+                console.log(results);
+            }
+        });
     }
 });
+
+// Keep RSSI between -60 and -80
