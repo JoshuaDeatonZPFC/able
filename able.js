4966c67d3fce8fd05dafa8c1cedf90770a72560c
JoshuaDeaton <jdeaton422@tech-rift.com>
2015-09-04 14:25:33 -0500

    Stable Scanning

--- able.js
+++ able.js
@@ -1,13 +1,15 @@
 function calcDistance(rssi) {
     var focal_point, distance;
-    if (rssi <= -70) {
+    if (rssi >= 70) {
         focal_point = -17;
-    } else if (rssi <= -60) {
+    } else if (rssi >= 60) {
         focal_point = -16;
-    } else if (rssi <= -50) {
+    } else if (rssi >= 50) {
         focal_point = -18;
+    } else {
+        focal_point = -16;
     }
-    distance = Math.pow(10, (focal_point - rssi) / (10 * 2));
+    distance = Math.pow(10, (focal_point + rssi) / (10 * 2));
     return parseInt(distance);
 }
 
@@ -23,14 +25,32 @@ var AIL = "s0",
     THR = "s2",
     RUD = "s3",
     AUX = "s4";
+var rssi1 = [],
+    rssi2 = [];
 
 console.log("App Start");
 
 sensor1.stdout.on('data', function (data) {
-    console.log("Sensor 1: " + data);
+    if (rssi1.length < 10) {
+        rssi1.push(Math.abs(data));
+    } else {
+        var minimum = Math.min.apply(Math, rssi1);
+        rssi1.splice(rssi1.indexOf(minimum), 1);
+        var newLocation = Math.min.apply(Math, rssi1);
+        console.log("Sensor 1 Location: " + calcDistance(newLocation) + " cm");
+        rssi1 = [];
+    }
 });
 sensor2.stdout.on('data', function (data) {
-    console.log("Sensor 2: " + data);
+    if (rssi2.length < 10) {
+        rssi2.push(Math.abs(data));
+    } else {
+        var minimum = Math.min.apply(Math, rssi2);
+        rssi2.splice(rssi2.indexOf(minimum), 1);
+        var newLocation2 = Math.min.apply(Math, rssi2);
+        console.log("Sensor 2 Location: " + calcDistance(newLocation2) + " cm");
+        rssi2 = [];
+    }
 });
 /*
 sensor3.stdout.on('data',function(data){
