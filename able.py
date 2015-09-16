e7d9db6617d7e1affa57f36ff80d16d2b8aa2325
JoshuaDeaton <jdeaton422@tech-rift.com>
2015-09-16 00:21:14 -0500

    Python Conversion Stage 3

--- able.py
+++ able.py
@@ -1,11 +1,31 @@
 import blescan
 import sys
+import math
+import time
+import serial
 
 import bluetooth._bluetooth as bluez
+
+s = serial.Serial("/dev/ttyS2",9600)
+
+def calcDistance(rssi):
+    focal_point = -16;
+    distance = math.pow(10, (focal_point + rssi) / (10 * 2));
+    return float("{0:.2f}".format(distance));
+
+def triangle(a,b,c):
+    temp = (math.acos((math.pow(a, 2) + math.pow(c, 2) - math.pow(b, 2)) / (2 * a * c))) * (180 / math.pi));
+    if temp > 90:
+        return ["left",(math.cos(180 - temp) * a) + 15,math.tan(180 - temp) * a]
+    elif temp <= 90 and temp >= 45:
+        return ["middle",(math.cos(temp) * a),math.tan(temp) * a]
+    elif temp < 45:
+        return ["right",(math.cos(temp) * a),math.tan(temp) * a]
 
 try:
     sensor_1 = bluez.hci_open_dev(0)
     sensor_2 = bluez.hci_open_dev(1)
+    sensor_3 = bluez.hci_open_dev(2)
 
 except:
     print "error accessing bluetooth device..."
@@ -15,21 +35,20 @@ blescan.hci_le_set_scan_parameters(sensor_1)
 blescan.hci_enable_le_scan(sensor_1)
 blescan.hci_le_set_scan_parameters(sensor_2)
 blescan.hci_enable_le_scan(sensor_2)
+blescan.hci_le_set_scan_parameters(sensor_3)
+blescan.hci_enable_le_scan(sensor_3)
 
 while True:
-    returnedList = blescan.parse_events(sensor_1, 1)
-    for beacon in returnedList:
-        print "Sensor_1"
-        print "UUID: "+beacon[len(beacon)-49]+beacon[len(beacon)-48]+beacon[len(beacon)-47]+beacon[len(beacon)-46]+beacon[len(beacon)-45]+beacon[len(beacon)-44]+beacon[len(beacon)-43]+beacon[len(beacon)-42]+beacon[len(beacon)-41]+beacon[len(beacon)-40]+beacon[len(beacon)-39]+beacon[len(beacon)-38]+beacon[len(beacon)-37]+beacon[len(beacon)-36]+beacon[len(beacon)-35]+beacon[len(beacon)-34]+beacon[len(beacon)-33]+beacon[len(beacon)-32]+beacon[len(beacon)-31]+beacon[len(beacon)-30]+beacon[len(beacon)-29]+beacon[len(beacon)-28]+beacon[len(beacon)-27]+beacon[len(beacon)-26]+beacon[len(beacon)-25]+beacon[len(beacon)-24]+beacon[len(beacon)-23]+beacon[len(beacon)-22]+beacon[len(beacon)-21]+beacon[len(beacon)-20]+beacon[len(beacon)-19]+beacon[len(beacon)-18]
-        print "Major: "+beacon[len(beacon)-16]+beacon[len(beacon)-15]+beacon[len(beacon)-14]
-        print "Minor: "+beacon[len(beacon)-12]+beacon[len(beacon)-11]+beacon[len(beacon)-10]+beacon[len(beacon)-9]
-        print "RSSI: "+beacon[len(beacon)-3]+beacon[len(beacon)-2]+beacon[len(beacon)-1]
 
-    returnedList = blescan.parse_events(sensor_2, 1)
-    for beacon in returnedList:
-        print "Sensor_2"
-        print "UUID: "+beacon[len(beacon)-49]+beacon[len(beacon)-48]+beacon[len(beacon)-47]+beacon[len(beacon)-46]+beacon[len(beacon)-45]+beacon[len(beacon)-44]+beacon[len(beacon)-43]+beacon[len(beacon)-42]+beacon[len(beacon)-41]+beacon[len(beacon)-40]+beacon[len(beacon)-39]+beacon[len(beacon)-38]+beacon[len(beacon)-37]+beacon[len(beacon)-36]+beacon[len(beacon)-35]+beacon[len(beacon)-34]+beacon[len(beacon)-33]+beacon[len(beacon)-32]+beacon[len(beacon)-31]+beacon[len(beacon)-30]+beacon[len(beacon)-29]+beacon[len(beacon)-28]+beacon[len(beacon)-27]+beacon[len(beacon)-26]+beacon[len(beacon)-25]+beacon[len(beacon)-24]+beacon[len(beacon)-23]+beacon[len(beacon)-22]+beacon[len(beacon)-21]+beacon[len(beacon)-20]+beacon[len(beacon)-19]+beacon[len(beacon)-18]
-        print "Major: "+beacon[len(beacon)-16]+beacon[len(beacon)-15]+beacon[len(beacon)-14]
-        print "Minor: "+beacon[len(beacon)-12]+beacon[len(beacon)-11]+beacon[len(beacon)-10]+beacon[len(beacon)-9]
-        print "RSSI: "+beacon[len(beacon)-3]+beacon[len(beacon)-2]+beacon[len(beacon)-1]
+    output_1 = ''.join(map(str, blescan.parse_events(sensor_1, 1)))
+    output_2 = ''.join(map(str, blescan.parse_events(sensor_2, 1)))
+    output_3 = ''.join(map(str, blescan.parse_events(sensor_3, 1)))
 
+    print output_1
+    s.open()
+    s.write("st\n") # Servo test command
+    time.sleep(5) # Wait for 5 seconds
+    s.write("serr\n") # Wrong command to stop servo test
+    s.close()
+    print output_2
+    print output_3
